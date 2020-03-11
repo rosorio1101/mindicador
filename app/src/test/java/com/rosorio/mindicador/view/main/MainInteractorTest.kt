7ebf0f2b4b335b8b5eba85ed84168651ee2e70fc
Roberto Osorio <robertoosorio.s@gmail.com>
2020-03-11 06:39:01 -0300

    improve coverage

--- app/src/test/java/com/rosorio/mindicador/view/main/MainInteractorTest.kt
+++ app/src/test/java/com/rosorio/mindicador/view/main/MainInteractorTest.kt
@@ -3,6 +3,8 @@ package com.rosorio.mindicador.view.main
 import com.rosorio.mindicador.api.MindicadorApi
 import com.rosorio.mindicador.api.MindicadorResponse
 import com.rosorio.mindicador.model.Indicator
+import okhttp3.MediaType
+import okhttp3.ResponseBody
 import org.junit.Before
 import org.junit.Test
 import org.mockito.InjectMocks
@@ -54,5 +56,53 @@ class MainInteractorTest {
         interactor.retrieveIndicators(mockListener)
 
         verify(mockListener).onSuccess(anyList())
+    }
+
+    @Test
+    fun `should retrieve error by error response`() {
+        val mockCall: Call<MindicadorResponse> = mock(Call::class.java) as Call<MindicadorResponse>
+        val mockMindicadorResponse = MindicadorResponse(
+            version = "",
+            autor = "",
+            fecha = null,
+            uf = Indicator(
+                "a",
+                "a",
+                "Pesos",
+                Date(),
+                1.0
+            )
+        )
+        val mockListener = mock(MainInteractor.OnRetrieveDataListener::class.java)
+
+        `when`(api.getIndicators()).thenReturn(mockCall)
+
+        doAnswer {
+            (it.arguments[0] as Callback<MindicadorResponse>).onResponse(mockCall, Response.error(400,
+                ResponseBody.create(MediaType.get("application/json"), "{\"error\":\"custom error\"}")
+            ))
+            null
+        }.`when`(mockCall).enqueue(any())
+
+        interactor.retrieveIndicators(mockListener)
+
+        verify(mockListener).onError(anyString())
+    }
+
+    @Test
+    fun `should retrieve error`() {
+        val mockCall: Call<MindicadorResponse> = mock(Call::class.java) as Call<MindicadorResponse>
+        val mockListener = mock(MainInteractor.OnRetrieveDataListener::class.java)
+
+        `when`(api.getIndicators()).thenReturn(mockCall)
+
+        doAnswer {
+            (it.arguments[0] as Callback<MindicadorResponse>).onFailure(mockCall, Exception("Custom message"))
+            null
+        }.`when`(mockCall).enqueue(any())
+
+        interactor.retrieveIndicators(mockListener)
+
+        verify(mockListener).onError(anyString())
     }
 }
